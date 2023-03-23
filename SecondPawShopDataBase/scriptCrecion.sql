5ff3913c219a9572babcc81543ca52074e4efe98
AndresSArias <andres31321@gmail.com>
2023-03-23 00:57:45 -0500

    [Create] DataBase script and fill

---
+++ SecondPawShopDataBase/scriptCrecion.sql
@@ -0,0 +1,141 @@
+/* ---------------------------------------------------- */
+/*              Creado por JSJSOLUTIONS	                */
+/* Script que crea la base de datos con sus objetos.    */
+/* ---------------------------------------------------- */
+
+/*Creación de la base de datos de SecondPawShop*/
+CREATE DATABASE IF NOT EXISTS  dbSecondPawShop;
+
+/*Establecer la base de datos para hacer las operaciones de creación*/
+USE dbSecondPawShop;
+
+/*Se desactiva temporalmente la verificación de llaves foraneas para la creación de todas la bd*/
+SET FOREIGN_KEY_CHECKS=0; 
+
+/* Drop de las tablas si existe */
+DROP TABLE IF EXISTS Producto CASCADE;
+
+DROP TABLE IF EXISTS Usuario CASCADE;
+
+DROP TABLE IF EXISTS Venta CASCADE;
+
+/* Creación de las tablas */
+
+CREATE TABLE Producto
+(
+	idUsuarioFK VARCHAR(15) NOT NULL,
+	nombre VARCHAR(50) NOT NULL,
+	categoria VARCHAR(10) NOT NULL,
+	descripcion VARCHAR(200) NOT NULL,
+	cantidad INT NOT NULL,
+	precio INT NOT NULL,
+	imagen VARCHAR(200) NOT NULL,
+	estado VARCHAR(15) NOT NULL,
+	CONSTRAINT PK_Table_One PRIMARY KEY (nombre ASC, idUsuarioFK ASC)
+)
+COMMENT = 'Tabla que contiene los registros de los productos de la tienda. ya sea que existan o no.'
+
+;
+
+CREATE TABLE Usuario
+(
+	idUsuario VARCHAR(15) NOT NULL COMMENT 'Clave primaria de la tabla Usuario',
+	nombre VARCHAR(50) NOT NULL COMMENT 'Columna que continene el nombre de los Usuarios',
+	apellido VARCHAR(50) NOT NULL COMMENT 'Columna que contiene los apellidos de los usuarios.',
+	correo VARCHAR(50) NOT NULL COMMENT 'Columna que contendrá los correos de los usuarios registrados',
+	celular VARCHAR(10) NOT NULL COMMENT 'Columna que contendrá el número de contacto de los usuarios',
+	direccion VARCHAR(50) NOT NULL COMMENT 'Columna que contiene los registros de las direcciones de los usuarios del sistema.',
+	contrasena VARCHAR(20) NOT NULL COMMENT 'Columna que contiene la contrasena del usuario para su autentificación en el sistema.',
+	CONSTRAINT PK_Table_Two PRIMARY KEY (idUsuario ASC)
+)
+COMMENT = 'Esta tabla contiene los registros de los clientes de la aplicación'
+
+;
+
+CREATE TABLE Venta
+(
+	idVenta VARCHAR(50) NOT NULL COMMENT 'id',
+	idUsuarioPropetario VARCHAR(50) NOT NULL,
+	nombreProducto VARCHAR(50) NOT NULL,
+	idUsuarioComprador VARCHAR(50) NOT NULL,
+	cantidadAComprar INT NOT NULL,
+	precioTotal INT NOT NULL,
+	estado VARCHAR(50) NOT NULL,
+	CONSTRAINT PK_Venta PRIMARY KEY (idVenta ASC, idUsuarioPropetario ASC, nombreProducto ASC, idUsuarioComprador ASC)
+)
+COMMENT = 'Tabla la cual contendrá los registros de compras por parte los usuarios del sistema y los productos en estado publicado.'
+
+;
+
+/* Creación de Primary Keys, Indexes, Uniques, Checks */
+/*Se crear los checks para la tablas producto*/
+ALTER TABLE Producto 
+ ADD CONSTRAINT Check_estado CHECK (estado IN ('VERIFICANDO', 'PUBLICADO', 'ACABADO'))
+;
+
+ALTER TABLE Producto 
+ ADD CONSTRAINT Check_categoria CHECK (categoria IN ('Salud', 'Bienestar', 'Ropa', 'Accesorio'))
+;
+/*Se crea el index de la tabla producto*/
+ALTER TABLE Producto 
+ ADD INDEX IXFK_Producto_Usuario (idUsuarioFK ASC)
+;
+
+/*Se crea un trigger de la tabla producto que cuando se actualice su catidad = 0, se cambie el estado a acabado*/
+DELIMITER $$
+CREATE TRIGGER actualizar_estado_producto
+BEFORE UPDATE ON Producto
+FOR EACH ROW
+BEGIN
+    IF NEW.cantidad = 0 THEN
+        SET NEW.estado = 'ACABADO';
+    END IF;
+END$$
+DELIMITER ;
+
+/*Se agrega el check de la tabla venta*/
+ALTER TABLE Venta 
+ ADD CONSTRAINT Check_estadoVenta CHECK (estado IN ('CARRO', 'CANCELADO', 'COMPRADO'))
+;
+/*Se agrega el index de la tabla venta con respecto a producto*/
+ALTER TABLE Venta 
+ ADD INDEX IXFK_Venta_Producto (nombreProducto ASC, idUsuarioPropetario ASC)
+;
+/*Se agrega el idenx de la tabla venta con respecto a usuario*/
+ALTER TABLE Venta 
+ ADD INDEX IXFK_Venta_Usuario (idUsuarioComprador ASC)
+;
+
+/*Se crea un trigger en la tabla venta que actualiza la cantidad de la tabla producto cuando en el estado este completado alguna venta*/
+DELIMITER $$
+CREATE TRIGGER actualizar_cantidad_producto
+AFTER INSERT ON Venta
+FOR EACH ROW
+BEGIN
+    UPDATE Producto
+    SET cantidad = cantidad - NEW.cantidadAComprar
+    WHERE Producto.idUsuarioFK = NEW.idUsuarioPropetario AND Producto.nombre = NEW.nombreProducto AND NEW.estado = 'COMPRADO';
+END
+$$
+DELIMITER ;
+
+
+/* Se crean los contraints de llaves foranes*/
+/*Llave forane de producto con respecto a usuar (relacion 1 a muchos) y que es debil, por eso es compuesta*/
+ALTER TABLE Producto 
+ ADD CONSTRAINT FK_Producto_Usuario
+	FOREIGN KEY (idUsuarioFK) REFERENCES Usuario (idUsuario) ON DELETE Restrict ON UPDATE Restrict
+;
+/*Se agrega la foranea de venta conrespecto a Producto, que tiene una primaria compuesta*/
+ALTER TABLE Venta 
+ ADD CONSTRAINT FK_Venta_Producto
+	FOREIGN KEY (nombreProducto, idUsuarioPropetario) REFERENCES Producto (nombre,idUsuarioFK) ON DELETE Restrict ON UPDATE Restrict
+;
+/*Se agrega la foranea de venta conrespecto a Usuario*/
+ALTER TABLE Venta 
+ ADD CONSTRAINT FK_Venta_Usuario
+	FOREIGN KEY (idUsuarioComprador) REFERENCES Usuario (idUsuario) ON DELETE Restrict ON UPDATE Restrict
+;
+
+/*Se reestable la comprobación de llaves foraneas*/
+SET FOREIGN_KEY_CHECKS=1; 
---
+++ SecondPawShopDataBase/scriptLlenado.sql
@@ -0,0 +1,52 @@
+/* ---------------------------------------------------- */
+/*              Creado por JSJSOLUTIONS	                */
+/* Script que hace el llenado de la base de datos.       */
+/* ---------------------------------------------------- */
+
+/*INSERCIÓN DE USUARIOS*/
+INSERT INTO USUARIO (idUsuario,nombre,apellido,correo,celular,direccion,contrasena) 
+VALUES ('1233906548','Andrés','Arias','andres31321@gmail.com','3142294642','Calle 133 #118-4','clave1');
+INSERT INTO USUARIO (idUsuario,nombre,apellido,correo,celular,direccion,contrasena) 
+VALUES ('4242123121','José','Yepés','yepes@gmail.com','3184282638','Carrera 24 #90-23','clave2');
+INSERT INTO USUARIO (idUsuario,nombre,apellido,correo,celular,direccion,contrasena) 
+VALUES ('7987897897','Santiago','Ventura','santiago@gmail.com','3123427615','Transversal 97 #26-30','clave3');
+INSERT INTO USUARIO (idUsuario,nombre,apellido,correo,celular,direccion,contrasena) 
+VALUES ('5754664764','Gabriel','Ventura','gabriel@gmail.com','3004123123','Avenida 23 #12-5','clave4');
+INSERT INTO USUARIO (idUsuario,nombre,apellido,correo,celular,direccion,contrasena) 
+VALUES ('1029384756','Dona','Parra','parraDona@gmail.com','3142294642','Calle 80 #126-52','Andrys');
+
+/*Inserción de PRODUCTOS*/
+INSERT INTO PRODUCTO (idUsuarioFK,nombre,categoria,descripcion,cantidad,precio,imagen,estado) 
+VALUES ('7987897897','Cama','Bienestar','Cama para perro o gato color café claro con poco uso',1,50000,'https://cdn.discordapp.com/attachments/1065754163205648404/1088253556051693690/cama.webp','PUBLICADO');
+INSERT INTO PRODUCTO (idUsuarioFK,nombre,categoria,descripcion,cantidad,precio,imagen,estado) 
+VALUES ('4242123121','Camisa','Ropa','Camisa para perro o gato talla m con poco uso',2,100000,'https://cdn.discordapp.com/attachments/1065754163205648404/1088255438878941194/camisa-p.jpg','PUBLICADO');
+INSERT INTO PRODUCTO (idUsuarioFK,nombre,categoria,descripcion,cantidad,precio,imagen,estado) 
+VALUES ('1233906548','Chalecos','Ropa','Chalecos para perros de tamaño pequeño, un poco usados',2,150000,'https://cdn.discordapp.com/attachments/1065754163205648404/1088257723503755274/chalecos.jpg','PUBLICADO');
+INSERT INTO PRODUCTO (idUsuarioFK,nombre,categoria,descripcion,cantidad,precio,imagen,estado) 
+VALUES ('1233906548','Collar de Cuero','Accesorio','Collar de cuero de alta calidad con poco uso',1,200000,'https://cdn.discordapp.com/attachments/1065754163205648404/1088259106160267284/collar-cuero.jpg','PUBLICADO');
+INSERT INTO PRODUCTO (idUsuarioFK,nombre,categoria,descripcion,cantidad,precio,imagen,estado) 
+VALUES ('5754664764','Disfraz','Ropa','Disfraz de una pieza con mucho uso y un precio de locura',1,50000,'https://cdn.discordapp.com/attachments/1065754163205648404/1088259974658007040/disfraz.jpg','PUBLICADO');
+INSERT INTO PRODUCTO (idUsuarioFK,nombre,categoria,descripcion,cantidad,precio,imagen,estado) 
+VALUES ('5754664764','Gafas Gato','Accesorio','Gafas para gato como nuevas',1,25000,'https://cdn.discordapp.com/attachments/1065754163205648404/1088260345199591524/gafas-gato.jpg','PUBLICADO');
+INSERT INTO PRODUCTO (idUsuarioFK,nombre,categoria,descripcion,cantidad,precio,imagen,estado) 
+VALUES ('4242123121','Impermeable','Ropa','Impermeable para lluvias casi nuevo',4,80000,'https://cdn.discordapp.com/attachments/1065754163205648404/1088261238431154248/impermeable.jpg','PUBLICADO');
+INSERT INTO PRODUCTO (idUsuarioFK,nombre,categoria,descripcion,cantidad,precio,imagen,estado) 
+VALUES ('4242123121','Juguete Perro','Bienestar','Juguestes para perro mordiclables con un poco de uso',3,20000,'https://cdn.discordapp.com/attachments/1065754163205648404/1088261806746783864/jugueteperro.jpg','PUBLICADO');
+INSERT INTO PRODUCTO (idUsuarioFK,nombre,categoria,descripcion,cantidad,precio,imagen,estado) 
+VALUES ('1233906548','Pastilla Antipulgas','Salud','Pastilla comestible para el contro de pulgas',5,12000,'https://cdn.discordapp.com/attachments/1065754163205648404/1088328266840027216/pastilla-antipulgas.jpg','PUBLICADO');
+INSERT INTO PRODUCTO (idUsuarioFK,nombre,categoria,descripcion,cantidad,precio,imagen,estado) 
+VALUES ('5754664764','Saco','Ropa','Saco con poco uso muy comodo para perritos mediano',1,65000,'https://cdn.discordapp.com/attachments/1065754163205648404/1088329107080753152/saco.jpg','PUBLICADO');
+INSERT INTO PRODUCTO (idUsuarioFK,nombre,categoria,descripcion,cantidad,precio,imagen,estado) 
+VALUES ('1233906548','Saco rojo','Ropa','Saco para perro pequeño, de color rojo es casi nuevo',1,95000,'https://cdn.discordapp.com/attachments/1065754163205648404/1088330146974531634/sacoPerro.jpg','VERIFICANDO');
+
+/*INSERCION DE VENTAS*/
+INSERT INTO VENTA (idVenta,idUsuarioPropetario,nombreProducto,idUsuarioComprador,cantidadAComprar,precioTotal,estado)
+VALUES ('1','1233906548','Pastilla Antipulgas','1029384756',1,12000,'COMPRADO');
+INSERT INTO VENTA (idVenta,idUsuarioPropetario,nombreProducto,idUsuarioComprador,cantidadAComprar,precioTotal,estado)
+VALUES ('2','4242123121','Impermeable','1233906548',1,80000,'COMPRADO');
+INSERT INTO VENTA (idVenta,idUsuarioPropetario,nombreProducto,idUsuarioComprador,cantidadAComprar,precioTotal,estado)
+VALUES ('3','4242123121','Impermeable','5754664764',1,80000,'COMPRADO');
+INSERT INTO VENTA (idVenta,idUsuarioPropetario,nombreProducto,idUsuarioComprador,cantidadAComprar,precioTotal,estado)
+VALUES ('3','1233906548','Pastilla Antipulgas','5754664764',1,12000,'COMPRADO');
+INSERT INTO VENTA (idVenta,idUsuarioPropetario,nombreProducto,idUsuarioComprador,cantidadAComprar,precioTotal,estado)
+VALUES ('3','4242123121','Juguete Perro','5754664764',1,20000,'COMPRADO');
