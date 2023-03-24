60f8050b62b554491461841fa081c7950829dc2e
AndresSArias <andres31321@gmail.com>
2023-03-24 14:01:37 -0500

    [Create] base of backend in Spring Boot

---
+++ BackSecondPawShop/src/main/java/com/secondpawshop/init/BackSecondPawShopApplication.java
@@ -0,0 +1,13 @@
+package com.secondpawshop.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSecondPawShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackSecondPawShopApplication.class, args);
+	}
+
+}
---
+++ BackSecondPawShop/src/test/java/com/secondpawshop/init/BackSecondPawShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secondpawshop.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSecondPawShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
